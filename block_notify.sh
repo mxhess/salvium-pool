1d18239f89f2fea53f11bdb8a8fb23c58ba41b79
Matt Hess <mxhess@gmail.com>
2025-08-01 15:05:00 +0000

    update block notify to use main pid from systemd

--- block_notify.sh
+++ block_notify.sh
@@ -5,8 +5,16 @@
 # Log the block notification
 echo "$(date): New block notification: $1" >> /home/salvium/logs/block_notify.log
 
+# Get the main pid
+MAIN_PID=$(systemctl show --property MainPID --value salvium-pool-leaf)
+
 # Signal the pool process to refresh block template
-pkill -USR1 -f "salvium-pool"
+if [ -n "$MAIN_PID" ] && [ "$MAIN_PID" != "0" ]; then
+    kill -USR1 $MAIN_PID
+    echo "$(date): Sent USR1 signal to PID $MAIN_PID" >> /home/salvium/logs/block_notify.log
+else
+    echo "$(date): ERROR - Could not get valid PID from systemd" >> /home/salvium/logs/block_notify.log
+fi
 
 exit 0
 
